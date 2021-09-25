93a8af8aa1b5b7747edf05455a72b641013ebc96
Kain991 <kainarberkin03012003@gmail.com>
2021-09-25 21:11:40 +0600

    Creating vector of objects with size 5 setting their parameters and giving random luck points

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <time.h>
 using namespace std;
 class Person{
 private:
@@ -39,6 +42,14 @@ public:
 
 };
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    vector <Person> person(5);
+    srand(time(0));
+    person[0].setAge(15);person[0].setName("Kainar");person[0].setLuck(rand()%100);
+    person[1].setAge(17);person[1].setName("Maksat");person[1].setLuck(rand()%100);
+    person[2].setAge(18);person[2].setName("Dosya");person[2].setLuck(rand()%100);
+    person[3].setAge(20);person[3].setName("Didar");person[3].setLuck(rand()%100);
+    person[4].setAge(25);person[4].setName("Yerassyl");person[4].setLuck(rand()%100);
+    for(int i = 0 ; i < person.size();i++){
+        cout<<person[i].getName()<<" "<<person[i].getAge()<<" "<<person[i].getLuck()<<"\n";
+    }
 }
