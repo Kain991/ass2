658f4d7ddac194a5e4decc0a674839f7af2a5a5b
Kain991 <kainarberkin03012003@gmail.com>
2021-09-25 18:03:38 +0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/ASS2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ASS2.exe"
+  "ASS2.exe.manifest"
+  "ASS2.pdb"
+  "CMakeFiles/ASS2.dir/main.cpp.obj"
+  "libASS2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ASS2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
